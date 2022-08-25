8413bba66214d9e95369d118a65bda604a1e4545
Satyajit Barik <satyajit.barik@unilever.com>
2022-08-25 17:11:14 +0530

    updated

--- core/settings.py
+++ core/settings.py
@@ -25,8 +25,8 @@ DEBUG = env('DEBUG')
 ASSETS_ROOT = os.getenv('ASSETS_ROOT', '/static/assets') 
 
 # load production server from .env
-ALLOWED_HOSTS        = ['localhost', 'localhost:85', '127.0.0.1',               env('SERVER', default='127.0.0.1') ]
-CSRF_TRUSTED_ORIGINS = ['http://localhost:85', 'http://127.0.0.1', 'https://' + env('SERVER', default='127.0.0.1') ]
+ALLOWED_HOSTS        = ['https://feulegoapp.azurewebsites.net/', 'https://feulegoapp.azurewebsites.net']
+CSRF_TRUSTED_ORIGINS = ['https://feulegoapp.azurewebsites.net/', 'https://feulegoapp.azurewebsites.net']
 
 # Application definition
 
