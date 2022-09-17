bc386912e72966fec8854c59436a38c075e677b3
Satyajit Barik <satyajit.barik@unilever.com>
2022-09-17 22:44:57 +0530

    graph chart added

--- apps/home/views.py
+++ apps/home/views.py
@@ -11,6 +11,8 @@ from databricks_cli.sdk.api_client import ApiClient
 from databricks_cli.dbfs.api import DbfsApi
 from databricks_cli.dbfs.dbfs_path import DbfsPath
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 from core.settings import DATABRICKS_HOST,DATABRICKS_TOKEN
 
@@ -57,17 +59,14 @@ def tables_data(request):
     # dbfs_api  = DbfsApi(api_client)
     # dbfs_path = DbfsPath(dbfs_source_file_path)
     # dbfs_api.get_file(dbfs_path, local_file_download_path, overwrite = True)
-    data = pd.read_parquet(local_file_download_path, engine='pyarrow').head(100)
-    data.rename(columns={'GLI Code': 'GLICode', 'UOM Macro': 'UOMMacro', 'EU001 OC':'EU001OC', 'DS OC':'DSOC'}, inplace=True)
+    data = pd.read_parquet(local_file_download_path, engine='pyarrow').head(10)
     data.head(10)
-    data = pd.read_csv(local_file_download_path).head(50)
-    data.rename(columns={'GLI Code': 'GLICode', 'UOM Macro': 'UOMMacro', 'EU001 OC':'EU001OC', 'DS OC':'DSOC'}, inplace=True)
-    data = data.to_html()
-    # parsing the DataFrame in json format.
-    print(data.head(10))
     json_records = data.reset_index().to_json(orient ='records')
     data = []
     data = json.loads(json_records)
     context = {'data': data}
+    df = pd.DataFrame(data)
+    df.set_index('index', inplace=True)
+    sns.barplot(data=df, x='Year_Week', y='SalesCSVolume')
+    plt.savefig('/home/satyajit/Desktop/opensource/lego-web/lego-admin/apps/static/assets/pic.png')
     return render(request, "home/tables-data.html", context)
-
