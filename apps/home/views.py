a64d545a0ade5884cf5186233423cc3e7e1c27ac
Satyajit Barik <satyajit.barik@unilever.com>
2022-11-11 15:30:59 +0530

    training model form

--- apps/home/views.py
+++ apps/home/views.py
@@ -14,7 +14,16 @@ import pandas as pd
 # import seaborn as sns
 # import matplotlib.pyplot as plt
 # pd.set_option("display.max_columns", 200)
-import eda
+import eda, ctfrv2
+import numpy as np
+from pyspark.sql import functions as F
+from IPython.display import set_matplotlib_formats
+# set_matplotlib_formats('retina')
+# pd.set_option("display.max_columns", 100)
+# pd.set_option("display.max_rows", 100)
+
+
+
 # try:
 #     import dask.dataframe as dd
 # except Exception as e:
@@ -165,28 +174,127 @@ def training_model(request):
     data = json.loads(json_records.to_json(orient ='records'))
     context = {'data': data, 'message': 'data loaded successfully.'}
     if request.method == 'POST':
-        id_col = request.POST.get('id_col')
-        target_col = request.POST.get('target_col')
-        time_index_col = request.POST.get('time_index_col')
-        file_name = request.POST.get('file_name')
-        download_path = request.POST.get('download_path')
-        static_cat_col_list = request.POST.getlist('static_cat_col_list')
-        temporal_known_num_col_list = request.POST.getlist('temporal_known_num_col_list')
-        temporal_known_cat_col_list = request.POST.getlist('temporal_known_cat_col_list')
-        sort_col_list = request.POST.getlist('sort_col_list')
-        amz_columns_dict = {'id_col': id_col,
-                        'target_col': target_col,
-                        'time_index_col': time_index_col,
-                        'static_num_col_list': [],
-                        'static_cat_col_list': static_cat_col_list,
-                        'temporal_known_num_col_list':  temporal_known_num_col_list,
-                        'temporal_unknown_num_col_list': [],
-                        'temporal_known_cat_col_list': temporal_known_cat_col_list,
-                        'temporal_unknown_cat_col_list': [],
-                        'strata_col_list': [],
-                        'sort_col_list': sort_col_list,
-                        'wt_col': None,
-                        }
+
+        key_col = 'cpf'
+        time_index_col = 'G_WEEK'
+        target_col = 'PHY_CS'
+        event_cols = ['prime_day']
+        date_cols = ['W']
+        promo_num_cols = [ 'Percent_Discount']
+        static_cat_cols = ['BrandCode']
+        known_num_cols = ['Holidays']
+        unknown_num_cols = []
+        strata_cols = ['TrainGroup']
+
+        amz_columns_dict = {'id_col': key_col,
+                    'target_col': target_col,
+                    'time_index_col': time_index_col,
+                    'static_num_col_list': [],
+                    'static_cat_col_list': static_cat_cols,
+                    'temporal_known_num_col_list': event_cols + known_num_cols + promo_num_cols,
+                    'temporal_unknown_num_col_list': [],
+                    'temporal_known_cat_col_list': date_cols,
+                    'temporal_unknown_cat_col_list': [],
+                    'strata_col_list': [],
+                    'sort_col_list': [key_col,time_index_col],
+                    'wt_col': None}
         print('amz_columns_dict-------->', amz_columns_dict)
-        
+
+        data_obj = ctfrv2.ctfrv2_dataset(col_dict=amz_columns_dict, 
+                window_len=26, 
+                fh=13, 
+                batch=16, 
+                min_nz=0,
+                PARALLEL_DATA_JOBS=2, 
+                PARALLEL_DATA_JOBS_BATCHSIZE=128)
+
+        train_till = 202152
+        test_till = 202213
+
+        # history/forecast cutoff
+        history_till = 202213
+        future_till = 202226
+
+        # Create Train/Test Dataset
+
+        trainset, testset = data_obj.train_test_dataset(df, train_till=train_till, test_till=test_till)
+        train_steps_per_epoch = 5
+
+        for i, (x,y,s,w) in enumerate(trainset):
+            if i > train_steps_per_epoch:
+                break
+            else:
+                print("step: ", i, x.shape, y.shape, s.shape, w.shape)
+     
+        # create infer dataset
+        infer_dataset, actuals_df = data_obj.infer_dataset(df, history_till=history_till, future_till=future_till)
+
+        baseline_infer_dataset = data_obj.baseline_infer_dataset(df, 
+                            history_till=history_till, 
+                            future_till=future_till,
+                            ignore_cols=promo_num_cols, 
+                            ignore_pad_values=[0]*len(promo_num_cols))
+
+        # Additional Model Inputs
+        col_index_dict = data_obj.col_index_dict
+        vocab = data_obj.vocab_list(df)
+
+        # define loss type & loss function
+        #loss_fn = ctfrv2.Normal_NLL_Loss(sample_weights=False)
+        #loss_fn = ctfrv2.Poisson_NLL_Loss(sample_weights=False)
+        #loss_fn = ctfrv2.Huber(delta=0.8, sample_weights=False)
+        #loss_fn = ctfrv2.QuantileLoss_v2(quantiles=[0.6], sample_weights=False)
+
+        loss_type = 'Point' # ['Point','Quantile','Negbin','Poisson','Normal']
+        # sample_weights = ['True','False']
+        loss_fn = ctfrv2.RMSE(sample_weights=False) # [ctfrv2.RMSE(sample_weights=sample_weights), ctfrv2.QuantileLoss_v2(quantiles = quantiles, sample_weights=sample_weights)]
+        # quantiles = [0.5, 0.6, 0.7, ...] # [0 - 1]
+
+        # build model
+
+        try:
+            del var_model
+        except:
+            pass
+            
+        var_model = ctfrv2.Feature_Weighted_ConvTransformer(col_index_dict = col_index_dict,
+                    vocab_dict = vocab,
+                    num_layers = 1,
+                    num_heads = 1,
+                    kernel_sizes = [1],
+                    d_model = 16,
+                    forecast_horizon = 13,
+                    max_inp_len = 13,
+                    loss_type = loss_type,
+                    num_quantiles = 1,             
+                    decoder_lags = 1,          
+                    dropout_rate=0.1)
+        print('var model----------------------------->', var_model)
+
+        var_model.build()
+
+        # Training specific parameters
+        # try:
+        #     best_var_model = var_model.train(trainset, 
+        #                             testset, 
+        #                             loss_function = loss_fn,              
+        #                             metric='MSE',  #['MSE','MAE'] -- selection from menu
+        #                             learning_rate=0.00003, # explicit entry by user
+        #                             max_epochs=1,  # rest all user eneters values
+        #                             min_epochs=1,
+        #                             train_steps_per_epoch=10,
+        #                             test_steps_per_epoch=5,
+        #                             patience=10,
+        #                             weighted_training=False,
+        #                             model_prefix='/home/satyajit/Desktop/opensource/session',
+        #                             logdir='/home/satyajit/Desktop/opensource/session')
+
+
+        #     var_model.model.summary()
+
+        # except Exception as e:
+        #     print('error is: {}'.format(e))
+
     return render(request, "home/data/training-model.html", context)
+
+
