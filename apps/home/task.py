5fb031f9b8a608257a12f0be80091fd2286d06d4
Satyajit Barik <satyajit.barik@unilever.com>
2023-01-03 10:29:05 +0530

    inference code added

--- apps/home/task.py
+++ apps/home/task.py
@@ -170,3 +170,93 @@ def async__training_task(amz_columns_dict,promo_num_cols,metric,learning_rate,nu
     except Exception as e:
         print('training_model error is: {}'.format(e))
     return 'training task complete'
+
+
+
+
+# @login_required(login_url="/login/")
+# def inference_flow(request):
+#     data = None
+#     user = request.user
+#     username = user.username
+#     #token = lib.auth()
+#     # adls_client = core.AzureDLFileSystem(token, store_name='bnlweda04d80242stgadls')
+#     # path = '/Unilever/satyajit/us_amz.csv'
+#     # mode = 'rb'
+#     # df = eda_flow_task.delay(path, mode)
+#     df = pd.read_csv("/home/satyajit/Desktop/opensource/data/us_amz.csv", low_memory=False)
+#     # with adls_client.open(path, mode) as f:
+#     #     df = pd.read_csv(f, low_memory=False)
+#     df = df.head(10)
+#     json_records = df.reset_index()
+#     data = []
+#     data = json.loads(json_records.to_json(orient ='records'))
+#     context = {'data': data, 'message': 'data loaded successfully.'}
+#     if request.method == 'POST':
+#         id_col = request.POST.get('id_col')
+#         target_col = request.POST.get('target_col')
+#         time_index_col = request.POST.get('time_index_col')
+#         file_name = request.POST.get('file_name')
+#         static_cat_col_list = request.POST.getlist('static_cat_col_list')
+#         temporal_known_num_col_list = request.POST.getlist('temporal_known_num_col_list')
+#         temporal_known_cat_col_list = request.POST.getlist('temporal_known_cat_col_list')
+#         sort_col_list = request.POST.getlist('sort_col_list')
+#         amz_columns_dict = {'id_col': id_col,
+#                         'target_col': target_col,
+#                         'time_index_col': time_index_col,
+#                         'static_num_col_list': [],
+#                         'static_cat_col_list': static_cat_col_list,
+#                         'temporal_known_num_col_list':  temporal_known_num_col_list,
+#                         'temporal_unknown_num_col_list': [],
+#                         'temporal_known_cat_col_list': temporal_known_cat_col_list,
+#                         'temporal_unknown_cat_col_list': [],
+#                         'strata_col_list': [],
+#                         'sort_col_list': sort_col_list,
+#                         'wt_col': None,
+#                         }
+#         print('amz_columns_dict-------->', amz_columns_dict)
+#         import os
+#         from core.settings import BASE_DIR
+#         download_path = os.path.join(BASE_DIR, "projects/eda/%s/" % username)
+#         if not os.path.exists(download_path):
+#             try:
+#                 os.makedirs(download_path)
+#                 print('folder created----->')
+#             except FileExistsError:
+#             # directory already exists
+#                 pass
+#         import uuid
+#         download_path = download_path 
+#         print('download_path------------->', download_path)
+#         uuid_no=uuid.uuid4().hex[:5]
+#         name_of_file =  file_name + '_' + str(uuid_no)         
+#         save_path = download_path
+#         # file_path = Path(save_path, name_of_file+".html")  
+#         file_path = os.path.join(save_path, name_of_file+".html")
+#         print('file_path--------->', file_path)
+#         file1 = open(file_path, "w")
+#         toFile = 'training_param'
+#         file1.write(toFile)
+#         file1.close()
+#         try:   
+#             status = async_task.delay(amz_columns_dict)
+#             print('status--------------->', status)
+#             # user = request.user
+#             # if user.email:
+#             #     from_email = settings.FROM_EMAIL
+#             #     recipient_email = user.email
+#             #     subject = 'EDA file generated'
+#             #     message = 'Your EDA file is generated successfully.'
+#             #     try:
+#             #         from django.core.mail import send_mail
+#             #         status = send_mail(subject, message, from_email, [recipient_email, ], fail_silently=False)
+#             #     except Exception as e:
+#             #         print('email error is ------>', e)
+#             #         return render(request,'home/index.html', {'message': 'email error'})
+#             # else:
+#             #     recipient_email = None 
+#             return render(request,'home/index.html', {'message': 'Eda Generated Complete'})
+#         except Exception as e:
+#             print('error is---->', e)
+#             return render(request,'home/index.html', {'message': 'Error while generating EDA'})
+#     return render(request, "home/data/eda-flow.html", context)
